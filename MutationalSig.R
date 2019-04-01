1171919b10acb376a5cf31de8c9310f6766b22e1
Wang Chengwei <504430784@qq.com>
2019-04-01 23:20:28 +0800

    function complete

--- MutationalSig.R
+++ MutationalSig.R
@@ -1,4 +1,3 @@
-
 # packages
 library(reshape2)
 library(BSgenome)
@@ -10,38 +9,45 @@ library(utils)
 library(deconstructSigs)
 
 maf_file = "/home/ninomoriaty/R_Project/311252_snv_indel.imputed.maf"
-basic_sample <- sample.mut.ref
+branch = c("311252-TC1", "311252-TP1", "311252-TC2", "311252-TP2-2", "311252-V", "311252-S")
 
-# read .maf file
-maf_input <- read.table(maf_file, quote = "", header = TRUE, fill = TRUE, sep = '\t')
-# get mutationalSigs-related  infomation
-dat.sample <- data.frame(maf_input[,ncol(maf_input)])
-dat.chr <- data.frame(as.character(maf_input[,2]), stringsAsFactors=FALSE)
-dat.chr[,1] <- paste("chr", dat.chr[,1], sep="")
-dat.pos <- maf_input[,3]
-dat.ref <- maf_input[,7]
-dat.alt <- maf_input[,9]
-mut.sig.ref <- data.frame(dat.sample, dat.chr, dat.pos, dat.ref, dat.alt)
-colnames(mut.sig.ref) <- c("Sample", "chr", "pos", "ref", "alt")
+# main function
+Mutational_Sigs_branch <- function(maf_file, branch){
+  # generate branch name
+  branch_name <- paste(branch, collapse = "+")
+  # read .maf file
+  maf_input <- read.table(maf_file, quote = "", header = TRUE, fill = TRUE, sep = '\t')
+  # get mutationalSigs-related  infomation
+  dat.sample <- data.frame(as.character(maf_input[,ncol(maf_input)]), stringsAsFactors=FALSE)
+  dat.sample[which(dat.sample[,1] %in% branch), 1] <- branch_name
+  dat.chr <- data.frame(as.character(maf_input[,2]), stringsAsFactors=FALSE)
+  dat.chr[,1] <- paste("chr", dat.chr[,1], sep="")
+  dat.pos <- maf_input[,3]
+  dat.ref <- maf_input[,7]
+  dat.alt <- maf_input[,9]
+  mut.sig.ref <- data.frame(dat.sample, dat.chr, dat.pos, dat.ref, dat.alt)
+  colnames(mut.sig.ref) <- c("Sample", "chr", "pos", "ref", "alt")
+  
+  # deconstructSigs
+  sigs.input <- mut.to.sigs.input(mut.ref = mut.sig.ref, 
+                                  sample.id = "Sample", 
+                                  chr = "chr", 
+                                  pos = "pos", 
+                                  ref = "ref", 
+                                  alt = "alt")
+  
+  sigs.which <- whichSignatures(tumor.ref = sigs.input, 
+                                signatures.ref = signatures.nature2013, 
+                                sample.id = branch_name,
+                                contexts.needed = TRUE)
+  
+  sigs.max <- colnames(sigs.which[["weights"]][which.max(sigs.which[["weights"]])])
+  sigs.max
+}
 
-# deconstructSigs
-sigs.input <- mut.to.sigs.input(mut.ref = mut.sig.ref, 
-                                sample.id = "Sample", 
-                                chr = "chr", 
-                                pos = "pos", 
-                                ref = "ref", 
-                                alt = "alt")
-
-sigs.which <- whichSignatures(tumor.ref = sigs.input, 
-                       signatures.ref = signatures.nature2013, 
-                       sample.id = "311252-S",
-                       contexts.needed = TRUE)
-
-sigs.max <- colnames(sigs.which[["weights"]][which.max(sigs.which[["weights"]])])
+# basic_sample <- sample.mut.ref
 
 plotSignatures(sigs.which, sub = 'example')
-
-makePie(sigs.which, sub = 'example')
 
 
 
