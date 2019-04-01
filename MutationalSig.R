c02f15168c6aaf40ecac3352a1cf8687352c5d6e
Wang Chengwei <504430784@qq.com>
2019-04-02 00:48:57 +0800

    redesign

--- MutationalSig.R
+++ MutationalSig.R
@@ -7,19 +7,31 @@ library(grDevices)
 library(graphics)
 library(utils)
 library(deconstructSigs)
+library(plyr)
 
 maf_file = "/home/ninomoriaty/R_Project/311252_snv_indel.imputed.maf"
-branch = c("311252-TC1", "311252-TP1", "311252-TC2", "311252-TP2-2", "311252-V", "311252-S")
+
+branch11 = data.frame(c("311252-S"))
+branch10 = data.frame(c("311252-TP2-2"))
+branch9 = data.frame(c("311252-TC2"))
+branch8 = data.frame(c("311252-TP1"))
+branch7 = data.frame(c("311252-TC1"))
+branch6 = data.frame(c("311252-V"))
+branch5 = data.frame(c("311252-TP2-2", "311252-S"))
+branch4 = data.frame(c("311252-TP1", "311252-TC2"))
+branch3 = data.frame(c("311252-TP1", "311252-TC2", "311252-V"))
+branch2 = data.frame(c("311252-TP1", "311252-TC2", "311252-TP2-2", "311252-V", "311252-S"))
+branch1 = data.frame(c("311252-TC1", "311252-TP1", "311252-TC2", "311252-TP2-2", "311252-V", "311252-S"))
+branches =  rbind.fill(branch1, branch2, branch3, branch4, branch5, branch6, branch7, branch8, branch9, branch10, branch11)
 
 # main function
-Mutational_Sigs_branch <- function(maf_file, branch){
-  # generate branch name
-  branch_name <- paste(branch, collapse = "+")
+# Usage: Mutational_Sigs_branch(maf_file, samples_vector)
+Mutational_sigs_tree <- function(maf_file, branches){
+
   # read .maf file
   maf_input <- read.table(maf_file, quote = "", header = TRUE, fill = TRUE, sep = '\t')
   # get mutationalSigs-related  infomation
   dat.sample <- data.frame(as.character(maf_input[,ncol(maf_input)]), stringsAsFactors=FALSE)
-  dat.sample[which(dat.sample[,1] %in% branch), 1] <- branch_name
   dat.chr <- data.frame(as.character(maf_input[,2]), stringsAsFactors=FALSE)
   dat.chr[,1] <- paste("chr", dat.chr[,1], sep="")
   dat.pos <- maf_input[,3]
@@ -28,6 +40,18 @@ Mutational_Sigs_branch <- function(maf_file, branch){
   mut.sig.ref <- data.frame(dat.sample, dat.chr, dat.pos, dat.ref, dat.alt)
   colnames(mut.sig.ref) <- c("Sample", "chr", "pos", "ref", "alt")
   
+
+}
+
+# Confirm sets of mutation
+Mutation_sets <- function(mut.sig.ref, branches){
+  # generate branch name
+  branch_name <- paste(branch, collapse = "+")
+  dat.sample[which(dat.sample[,1] %in% branch), 1] <- branch_name
+}
+  
+# Weight mutational Signature of each branch
+Mutational_sigs_branch <- function(mut.sets.sig.ref, branch){
   # deconstructSigs
   sigs.input <- mut.to.sigs.input(mut.ref = mut.sig.ref, 
                                   sample.id = "Sample", 
@@ -44,11 +68,6 @@ Mutational_Sigs_branch <- function(maf_file, branch){
   sigs.max <- colnames(sigs.which[["weights"]][which.max(sigs.which[["weights"]])])
   sigs.max
 }
-
-# basic_sample <- sample.mut.ref
-
-plotSignatures(sigs.which, sub = 'example')
-
 
 
 
