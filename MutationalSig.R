2b778c72b9523698af85c3c070f6ce4a8f52becc
Wang Chengwei <504430784@qq.com>
2019-04-01 17:42:24 +0800

    protoype

--- MutationalSig.R
+++ MutationalSig.R
@@ -10,21 +10,22 @@ library(utils)
 library(deconstructSigs)
 
 maf_file = "/home/ninomoriaty/R_Project/311252_snv_indel.imputed.maf"
-
+basic_sample <- sample.mut.ref
 
 # read .maf file
 maf_input <- read.table(maf_file, quote = "", header = TRUE, fill = TRUE, sep = '\t')
 # get mutationalSigs-related  infomation
 dat.sample <- data.frame(maf_input[,ncol(maf_input)])
-dat.chr <- maf_input[,2]
+dat.chr <- data.frame(as.character(maf_input[,2]), stringsAsFactors=FALSE)
+dat.chr[,1] <- paste("chr", dat.chr[,1], sep="")
 dat.pos <- maf_input[,3]
 dat.ref <- maf_input[,7]
-dat.alt <- maf_input[,ncol(maf_input)-4]
-sample.mut.ref <- data.frame(dat.sample, dat.chr, dat.pos, dat.ref, dat.alt)
-colnames(sample.mut.ref) <- c("Sample", "chr", "pos", "ref", "alt")
+dat.alt <- maf_input[,9]
+mut.sig.ref <- data.frame(dat.sample, dat.chr, dat.pos, dat.ref, dat.alt)
+colnames(mut.sig.ref) <- c("Sample", "chr", "pos", "ref", "alt")
 
 # deconstructSigs
-sigs.input <- mut.to.sigs.input(mut.ref = sample.mut.ref, 
+sigs.input <- mut.to.sigs.input(mut.ref = mut.sig.ref, 
                                 sample.id = "Sample", 
                                 chr = "chr", 
                                 pos = "pos", 
@@ -33,7 +34,8 @@ sigs.input <- mut.to.sigs.input(mut.ref = sample.mut.ref,
 
 sigs.which = whichSignatures(tumor.ref = sigs.input, 
                        signatures.ref = signatures.nature2013, 
-                       sample.id = 2)
+                       sample.id = "311252-S",
+                       contexts.needed = TRUE)
 
 plotSignatures(sigs.which, sub = 'example')
 
